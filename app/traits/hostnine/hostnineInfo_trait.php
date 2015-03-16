fb347f68bcc78385784f593fb16a686e1c682433
phn007 <citb.pl@gmail.com>
2015-03-16 00:37:34 -0700

    Edit Hostnineinfo trait

--- app/traits/hostnine/hostnineInfo_trait.php
+++ app/traits/hostnine/hostnineInfo_trait.php
@@ -5,13 +5,29 @@ trait HostnineInfo {
 	}
 
 	function getApiKey( $accountName ) {
-		$apiKeys = array(
-			'maxcom' => 'Uq5plhH6UUobqhhU1bTHca5DxUC4euto4',
-			'ratzin' => 'M2fXDzAWrbae2kGlXAknS4vdN2RhJV93b',
-			'rexce' => '9dG99tk8O9aepZ6PWo7uegfpEDnSdROb6'
-		);
-		if ( ! array_key_exists( $accountName, $apiKeys ) )
+		$accounts = $this->accounts();
+		if ( ! array_key_exists( $accountName, $accounts ) )
 			die( 'Hostnine: account name not found' );
-		return $apiKeys[$accountName];
+		return $accounts[$accountName]['api_key'];
+	}
+
+	function accounts() {
+		return array(
+			'maxcom' => array(
+				'user' => 'maxcom',
+				'password' => 'bbIX746r4q',
+				'api_key' => 'Uq5plhH6UUobqhhU1bTHca5DxUC4euto4',
+			),
+			'ratzin' => array(
+				'user' => 'mnyhhhlq',
+				'password' => '1ip28UI6wm',
+				'api_key' => 'M2fXDzAWrbae2kGlXAknS4vdN2RhJV93b',
+			),
+			'rexce' => array(
+				'user' => 'nbbqasol',
+				'password' => 'f0j40GC8rz',
+				'api_key' => '9dG99tk8O9aepZ6PWo7uegfpEDnSdROb6'
+			)
+		);
 	}
 }
